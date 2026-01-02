f5605897488d076969a4fa21ddd9542b8bd63ec6
Lloyd Everett <lloydeverett@gmail.com>
2026-01-02 13:01:12 +0200

    wip select input for parallax theme

--- main.js
+++ main.js
@@ -45,10 +45,35 @@ function updateClocks() {
 updateClocks();
 window.setInterval(updateClocks, 1000);
 
+/* parallax theme select */
+const parallaxThemeSelect = document.querySelector("#parallax-theme-select");
+const documentStyles = getComputedStyle(document.documentElement);
+for (const propertyName of documentStyles) {
+    if (!propertyName.startsWith("--parallax-theme-name-")) {
+        continue;
+    }
+    const themeName = documentStyles.getPropertyValue(propertyName).trim();
+    const option = document.createElement('option');
+    option.value = themeName;
+    option.textContent = themeName;
+    parallaxThemeSelect.appendChild(option);
+}
+
 /* floating toolbar toggle */
-document.querySelector("#float-toolbar").checked = false;
-document.querySelector("#float-toolbar").addEventListener("change", function() {
-    document.querySelector("body").classList.toggle("floating-toolbars", this.checked);
+const body = document.querySelector("body");
+const floatToolbarInput = document.querySelector("#float-toolbar");
+floatToolbarInput.checked = false;
+floatToolbarInput.addEventListener("change", function() {
+    body.classList.toggle("floating-toolbars", this.checked);
 });
 
+/* allow styling based on the width of select elements, by setting the length of the currently-selected item as an attribute */
+for (const select of document.querySelectorAll("select:not([multiple])")) {
+    select.style.setProperty('--selected-text-length', select.options[select.selectedIndex].text.length);
+    select.addEventListener('change', function() {
+        select.style.setProperty('--selected-text-length', select.options[select.selectedIndex].text.length);
+    });
+}
+
 })();
+
