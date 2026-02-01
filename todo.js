b9de139fabfa27c5a3e65dc5cc2c91864022808b
Lloyd Everett <lloydeverett@gmail.com>
2026-02-01 20:41:47 +0200

    misc improvements

--- todo.js
+++ todo.js
@@ -16,17 +16,20 @@ class TodoSurface extends LitElement {
         return this; // no shadow DOM
     }
     render() {
-        let title = this.todoTitle || this.parentElement.slideTitle;
         return html`
             <div class="surface layout-vertical layout-justify-center layout-align-center layout-contain md-obey-max-height obey-max-width obey-width obey-height" style="--width: 100%; height: 100%; --max-height: 45rem; --max-width: 70rem;">
                 <attach-snapping-carousel-navigation-left class="md"></attach-snapping-carousel-navigation-left>
                 <attach-snapping-carousel-navigation-right class="md"></attach-snapping-carousel-navigation-right>
-                <div class="themed-checkbox h1">
-                    <input disabled type="checkbox" id="${this._id}" name="${this._id}" ?checked="${this.done}">
-                    <label class="no-user-select" for="${this._id}" >${title}</h1>
-                </div>
+                <h1>${this.todoTitle}</h1>
             </div>
         `;
+        /*
+             TODO: Support for "done" attribute - starting with e.g.
+            <div class="themed-checkbox h1">
+                <input disabled type="checkbox" id="${this._id}" name="${this._id}" ?checked="${this.done}">
+                <label class="no-user-select" for="${this._id}" >${this.todoTitle}</label>
+            </div>
+        */
     }
 }
 customElements.define('todo-surface', TodoSurface);
